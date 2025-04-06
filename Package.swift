c580bb140045a1f237c9fc0d816e196f02268a71
Kamil <kamil@Kamils-Mac-mini.local>
2025-04-06 18:16:47 +0500

    updating ios deployment target

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "combine-plus",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v14),
         .macOS(.v13),
         .tvOS(.v13),
         .watchOS(.v10),
